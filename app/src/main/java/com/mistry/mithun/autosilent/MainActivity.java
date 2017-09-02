e8c4da640b7fb191cba337b0a1ff727fc86389ad
Mithun <mithunjmistry@gmail.com>
2017-09-02 19:45:30 -0400

    Permissions issue fixed for Android 6.0 and above.

--- app/src/main/java/com/mistry/mithun/autosilent/MainActivity.java
+++ app/src/main/java/com/mistry/mithun/autosilent/MainActivity.java
@@ -1,7 +1,15 @@
 package com.mistry.mithun.autosilent;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,11 +19,30 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Boolean exit = false;
+    private Boolean permission_error = false;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_NOTIFICATION_POLICY = 111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            NotificationManager n = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+            try {
+                if (!n.isNotificationPolicyAccessGranted()) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_NOTIFICATION_POLICY},
+                            MY_PERMISSIONS_REQUEST_ACCESS_NOTIFICATION_POLICY);
+                }
+            }
+            catch (NullPointerException e){
+                permission_error = true;
+            }
+            if(permission_error){
+                Toast.makeText(this, "Please allow permissions for the app to perform properly.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     public void set_schedule(View view){
@@ -69,5 +96,51 @@ public class MainActivity extends AppCompatActivity {
     public void wifisilent(View view){
         Intent intent = new Intent(this, WifiSilentActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_NOTIFICATION_POLICY: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    AlertDialog.Builder alert = new AlertDialog.Builder(
+                            MainActivity.this);
+                    alert.setTitle("Permission required!");
+                    alert.setMessage("Please allow DND permission for app to work properly.");
+                    alert.setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                            startActivity(intent);
+                        }
+                    });
+                    alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    alert.show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
